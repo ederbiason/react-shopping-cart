4769c2c9c096e3480ab1ee5b1d237a2a1ffd59cd
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-05-24 17:58:19 -0300

    default settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useQuery } from "react-query";
 
-function App() {
+import { Drawer, LinearProgress, Grid, Badge } from "@mui/material";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+
+import { Wrapper } from "./App.styles";
+
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+}
+
+const getProducts = async (): Promise<CartItemType[]> => 
+  await (await fetch('https://fakestoreapi.com/products')).json()
+
+export function App() {
+  const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts)
+
+  const getTotalItems = () => null
+
+  const handleAddToCart = () => null
+
+  const handleRemoveFromCart = () => null
+ 
+  if (isLoading) return <LinearProgress/>
+
+  if (error) return <div>Something went wrong ...</div>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello World
     </div>
   );
 }
-
-export default App;
