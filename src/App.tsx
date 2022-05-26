c231a731cf95f64c6adc3996e4e6021a168bad74
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-05-25 23:02:28 -0300

    item component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Drawer, LinearProgress, Grid, Badge } from "@mui/material";
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 import { Wrapper } from "./App.styles";
+import { Item } from "./Item";
 
 export type CartItemType = {
   id: number;
@@ -24,7 +25,7 @@ export function App() {
 
   const getTotalItems = () => null
 
-  const handleAddToCart = () => null
+  const handleAddToCart = (clickedItem: CartItemType) => null
 
   const handleRemoveFromCart = () => null
  
@@ -33,8 +34,14 @@ export function App() {
   if (error) return <div>Something went wrong ...</div>
 
   return (
-    <div>
-      Hello World
-    </div>
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map(item => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart}/>
+          </Grid>  
+        ))}
+      </Grid>
+    </Wrapper>
   );
 }
