4276a5d269e60d48847ea700c8b8efcc777aedf4
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-05-26 16:22:11 -0300

    remove item function and total

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,18 @@ export function App() {
     })
   }
 
-  const handleRemoveFromCart = () => null
+  const handleRemoveFromCart = (id: number) => {
+    setCartItems(prev =>
+      prev.reduce((ack, item) => {
+        if(item.id === id) {
+          if (item.amount === 1) return ack
+          return [...ack, { ...item, amount: item.amount - 1 }]
+        } else {
+          return [...ack, item]
+        }
+      }, [] as CartItemType[]) 
+    )
+  }
  
   if (isLoading) return <LinearProgress/>
 
--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -10,6 +10,9 @@ type Props = {
 }
 
 export const Cart: React.FC<Props> = ({cartItems, addToCart, removeFromCart}) => {
+    const calculateTotal = (items: CartItemType[]) => 
+        items.reduce((ack: number, item) => ack + item.amount * item.price, 0)
+
     return (
         <Wrapper>
             <h2>Your Shopping Cart</h2>
@@ -22,6 +25,7 @@ export const Cart: React.FC<Props> = ({cartItems, addToCart, removeFromCart}) =>
                     removeFromCart={removeFromCart}
                 />
             ))}
+            <h2>Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
         </Wrapper>
     )
 }
